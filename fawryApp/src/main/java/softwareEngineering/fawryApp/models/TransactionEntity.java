ad1eaab8bd827fc2055974ba2045ab9a95ab6158
Mohamed Amr Abd El Fatah <muhamedamr241@gmail.com>
2022-12-28 00:31:50 +0200

    transaction endpoint

--- fawryApp/src/main/java/softwareEngineering/fawryApp/models/TransactionEntity.java
+++ fawryApp/src/main/java/softwareEngineering/fawryApp/models/TransactionEntity.java
@@ -4,10 +4,12 @@ public class TransactionEntity {
 	public int transId;
 	public double amount;
 	public String serviceName;
+	public String serviceProvider;
 	public String userEmail;
-	public TransactionEntity(int transId, double amount,String serviceName, String userEmail) {
+	public TransactionEntity(int transId, double amount,String serviceProvider,String serviceName, String userEmail) {
 		this.transId = transId;
 		this.amount = amount;
+		this.serviceProvider=serviceProvider;
 		this.serviceName = serviceName;
 		this.userEmail = userEmail;
 	}
--- fawryApp/src/main/java/softwareEngineering/fawryApp/models/Transactions.java
+++ fawryApp/src/main/java/softwareEngineering/fawryApp/models/Transactions.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Transactions {
 	public static ArrayList<TransactionEntity> transactions = new ArrayList<TransactionEntity>();
 	public static Map<String, Integer> userTransactionNumber = new HashMap<String, Integer>();
-	public static void addTransaction(int id, double amount, String serviceName, String email)
+	public static void addTransaction(int id, double amount,String serviceProvider ,String serviceName, String email)
 	{
-		TransactionEntity obj = new TransactionEntity(id, amount, serviceName, email);
+		TransactionEntity obj = new TransactionEntity(id, amount,serviceProvider ,serviceName, email);
 		transactions.add(obj);
 	}
 
