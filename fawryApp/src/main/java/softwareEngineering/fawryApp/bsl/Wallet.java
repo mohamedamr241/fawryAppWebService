f854944a34caece3d5e8c992ffb72024ee10d07f
nermin <nerminhassan2820@gmail.com>
2022-12-31 04:44:55 +0200

    Update Wallet.java

--- fawryApp/src/main/java/softwareEngineering/fawryApp/bsl/Wallet.java
+++ fawryApp/src/main/java/softwareEngineering/fawryApp/bsl/Wallet.java
@@ -1,12 +1,11 @@
 package softwareEngineering.fawryApp.bsl;
 
-import java.util.Map;
-
 import softwareEngineering.fawryApp.models.Account;
+import softwareEngineering.fawryApp.models.User;
 
 public class Wallet implements Payment{ 
-	private double lastbalance = 0;
-	private double balance = 0;
+	private double previousBalance = 0, balance = 0;
+	
 	public double pay(double amount) {
 		balance-=amount;
 		return  amount;
@@ -14,24 +13,23 @@ public class Wallet implements Payment{
 	
 	public String chargeViaCreditCard(double balance)
 	{
-		lastbalance=this.balance;
+		String ret = "Your wallet balance is updated from " + previousBalance;
+		previousBalance = balance;
 		this.balance += balance;
-		return "wallet is charged with " + balance +" successfully.";
+		return ret + " $ to " + this.balance + " $ successfully.";
 	}
 	
 	public double getBalance()
 	{
 		return balance;
 	}
-	public double getLastBalance()
-	{
-		return lastbalance;
-	}
+
+	
 	public static Wallet getUserWallet(String email) {
-		for (Map.Entry<String, Wallet> entry : Account.userWallet.entrySet())
+		for (Account ac : User.getAccounts())
 		{
-			if(entry.getKey().equals(email))
-				return entry.getValue();
+			if(ac.email.equals(email))
+				return ac.wallet;
 		}
 		return null;
 	}
