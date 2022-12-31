3433bfcf4449acbb62431b5685132105772d4066
nermin <nerminhassan2820@gmail.com>
2022-12-31 03:36:41 +0200

    Update RefundController.java

--- fawryApp/src/main/java/softwareEngineering/fawryApp/controllers/RefundController.java
+++ fawryApp/src/main/java/softwareEngineering/fawryApp/controllers/RefundController.java
@@ -1,49 +1,52 @@
 package softwareEngineering.fawryApp.controllers;
 
-
 import java.util.ArrayList;
-
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import softwareEngineering.fawryApp.bsl.Admin;
-import softwareEngineering.fawryApp.bsl.Services;
-import softwareEngineering.fawryApp.bsl.User;
-import softwareEngineering.fawryApp.models.Account;
+import softwareEngineering.fawryApp.bsl.RefundBsl;
 import softwareEngineering.fawryApp.models.TransactionEntity;
-import softwareEngineering.fawryApp.models.Transactions;
+
+
 @RestController
 public class RefundController{
 
-	Admin admin = new Admin();
-	User user = new User();
+	private RefundBsl refundBsl;
+	
+	public RefundController(RefundBsl refundBsl)
+	{
+		this.refundBsl = refundBsl;
+	}
+	
+	
 	@PostMapping(value = "/user/requestRefund")
 	public String requestRefund(@RequestBody TransactionEntity obj) 
 	{
-		return user.requestRefund(obj);
+		return refundBsl.requestRefund(obj);
 	}
 	
 	@GetMapping(value = "/admin/refundRequests")
 	public ArrayList<TransactionEntity> refundRequests() {
-		return admin.getrefundRequestList();
+		return refundBsl.refund.getrefundRequestList();
+	}
+	
+	@GetMapping(value = "/admin/refundTransactions")
+	public ArrayList<TransactionEntity> refundTransaction() {
+		return refundBsl.refund.getrefundedTransactions();
 	}
 	
 	@GetMapping(value = "/admin/processRefund/{transId}")
 	public String proccessRefund(@PathVariable("transId") int transId)
 	{
-		return admin.processRefund(transId);
+		return refundBsl.processRefund(transId);
 	}
 	
-	@GetMapping(value = "/admin/processRefund/{transId}/{decision}")
+	@GetMapping(value = "/admin/processRefund/{transId}/{decision}")//decision->accept, reject
 	public String accOrRej(@PathVariable("transId") int transId, @PathVariable("decision") String decision)
 	{
-		return admin.accOrRej(decision, transId);
+		return refundBsl.accOrRej(decision, transId);
 	}
 
 }
